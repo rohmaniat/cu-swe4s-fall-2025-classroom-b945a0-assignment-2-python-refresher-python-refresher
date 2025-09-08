d12f6c1956fd6206d68b225cd1d2a17d7645d610
rohmaniat <84361227+rohmaniat@users.noreply.github.com>
2025-09-08 16:00:54 -0600

    Finished the get_column function

--- my_utils.py
+++ my_utils.py
@@ -1,2 +1,12 @@
 def get_column(file_name, query_column, query_value, result_column):
-    return None
+    open file_name as f #open the file
+
+    #Splits each line by column
+    #Checks if query_column matches query_value
+    #If match, it returns the value in result_column
+    for line in f:
+        columns = line.split(',')
+        if columns[query_column] == query_value:
+            return columns[result_column]
+    else:
+        return None
