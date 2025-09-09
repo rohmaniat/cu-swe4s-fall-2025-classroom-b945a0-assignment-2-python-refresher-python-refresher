bb57476a58c02aa5bc3c2b78e3b3ca927ba03e4e
rohmaniat <84361227+rohmaniat@users.noreply.github.com>
2025-09-09 12:26:18 -0600

    added an exception if the called file cannot be found

--- my_utils.py
+++ my_utils.py
@@ -1,10 +1,14 @@
 def get_column(file_name, query_column, query_value, result_column):
-    f = open(file_name, 'r') #open the file
     results = []
+    try:
+        f = open(file_name, 'r') # open the file
+    except FileNotFoundError:
+            print(f"Error: The file '{file_name}' was not found.")
+            return results # exit statement if the file cannot be opened
 
-    #Splits each line by column
-    #Checks if query_column matches query_value
-    #If match, it returns the value in result_column
+    # Splits each line by column
+    # Checks if query_column matches query_value
+    # If match, it returns the value in result_column
 
     for line in f:
         columns = line.strip().split(',')
@@ -13,4 +17,6 @@ def get_column(file_name, query_column, query_value, result_column):
 
     f.close()
     return results
+
+print(get_column('/Users/rohan/local_coding/assignment-2-python-refresher-rohmaniat/Agrofood_co2_emission.csv', 0, 'United States of America', 3))
 
