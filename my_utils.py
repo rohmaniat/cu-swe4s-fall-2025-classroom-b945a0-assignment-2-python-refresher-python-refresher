5d83cadef40ff7cf169046cac19fb541eafd675a
rohmaniat <84361227+rohmaniat@users.noreply.github.com>
2025-09-08 21:35:03 -0600

    added code to remove trailing whitespaces and close the file after the function is finished

--- my_utils.py
+++ my_utils.py
@@ -1,12 +1,16 @@
 def get_column(file_name, query_column, query_value, result_column):
     f = open(file_name, 'r') #open the file
+    results = []
 
     #Splits each line by column
     #Checks if query_column matches query_value
     #If match, it returns the value in result_column
+
     for line in f:
-        columns = line.split(',')
+        columns = line.strip().split(',')
         if columns[query_column] == query_value:
-            return columns[result_column]
-    else:
-        return None
+            results.append(columns[result_column])
+
+    f.close()
+    return results
+
