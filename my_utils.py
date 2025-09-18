c2be7d7b47ced362d035a527fe08cc1de6c15a25
rohmaniat <84361227+rohmaniat@users.noreply.github.com>
2025-09-18 14:56:29 -0600

    the output values are now converted to integers

--- my_utils.py
+++ my_utils.py
@@ -1,4 +1,4 @@
-def get_column(file_name, query_column, query_value, result_column = 1):
+def get_column(file_name, query_column, query_value, result_column):
     results = []
     try:
         f = open(file_name, 'r') # open the file
@@ -13,10 +13,26 @@ def get_column(file_name, query_column, query_value, result_column = 1):
     for line in f:
         columns = line.strip().split(',')
         if columns[query_column] == query_value:
-            results.append(int(columns[result_column]))
+            results.append(columns[result_column]) #TODO: convert outputs to integers
 
     f.close()
+
+    # Convert the string values to floats first
+    for i in range(0 , len(results)):
+        try:
+            results[i] = float(results[i])
+        except ValueError:
+            continue
+    # Then convert the floats to integers
+    for i in range(0 , len(results)):
+        try:
+            results[i] = int(results[i])
+        except ValueError:
+            continue
+    
     return results
 
-print(get_column('/Users/rohan/local_coding/assignment-2-python-refresher-rohmaniat/Agrofood_co2_emission.csv', 0, 'United States of America', 3))
-
+if __name__ == "__main__":
+    print(get_column('/Users/rohan/local_coding/assignment-2-python-refresher-rohmaniat/Agrofood_co2_emission.csv', 0, 'United States of America', 3))
+# only runs when the file is directly called
+# For testing my_utils.py independently
