e20793320def8639ac35bcbd46ece13cfc4422e0
rohmaniat <84361227+rohmaniat@users.noreply.github.com>
2025-09-18 15:01:41 -0600

    added the shebang to the top plus arguments

--- run.sh
+++ run.sh
@@ -1,4 +1,7 @@
-python3 print_fires.py
+#!/bin/bash
 
-# chmod +x run.sh to make the code executable
+python3 print_fires.py --file_name Agrofood_co2_emission.csv --country "United States of America" --country_column 0 --fires_column 3
+
+# chmod +x run.sh 
+# to make the code executable
 # ./run.sh to run the code
