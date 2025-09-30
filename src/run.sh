99b169a2eb9ca340bc20d725abb259ed2c07cad9
rohmaniat <84361227+rohmaniat@users.noreply.github.com>
2025-09-30 16:54:52 -0600

    updated the chosen file

--- src/run.sh
+++ src/run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # This one will work
-python3 src/print_fires.py --file_name Agrofood_co2_emission.csv --country "United States of America" --country_column 0 --fires_column 3 --operation "mean"
+python3 src/print_fires.py --file_name src/agrofood_data_abbreviated.csv --country "United States of America" --country_column 0 --fires_column 3 --operation "mean"
 
 # chmod +x run.sh 
 # to make the code executable
