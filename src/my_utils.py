dcc1c61a1dd8b354520cf436cbfdb420572d7368
rohmaniat <84361227+rohmaniat@users.noreply.github.com>
2025-10-07 12:07:00 -0600

    pycodestyle compliant as well

--- src/my_utils.py
+++ src/my_utils.py
@@ -1,12 +1,13 @@
 import sys
 
+
 def get_column(file_name, query_column, query_value, result_column):
     results = []
     try:
-        f = open(file_name, 'r') # open the file for reading
+        f = open(file_name, 'r')  # open the file for reading
     except FileNotFoundError:
-            print(f"Error: The file '{file_name}' was not found.")
-            return results # exit statement if the file cannot be opened
+        print(f"Error: The file '{file_name}' was not found.")
+        return results  # exit if file cannot be opened
 
     # Splits each line by column
     # Checks if query_column matches query_value
@@ -19,23 +20,24 @@ def get_column(file_name, query_column, query_value, result_column):
 
     f.close()
 
-    # Convert the string values to floats first
-    for i in range(0 , len(results)):
+    # Convert string values to floats first
+    for i in range(len(results)):
         try:
             results[i] = float(results[i])
         except ValueError:
             continue
-    
-    # Then convert the floats to integers
-    for i in range(0 , len(results)):
+
+    # Then convert floats to integers
+    for i in range(len(results)):
         try:
             results[i] = int(results[i])
         except ValueError:
             continue
-    
+
     return results
 
-def mean (arr):
+
+def mean(arr):
     # Calculate mean
     try:
         mean_out = sum(arr) / len(arr)
@@ -44,14 +46,15 @@ def mean (arr):
         return "Array is empty."
     except TypeError:
         return "Please use floats or strings! :)"
-    
-def median (arr):
+
+
+def median(arr):
     # Calculate median
     try:
         sorted_arr = sorted(arr)
     except TypeError:
         return "Please use floats or strings! :)"
-        
+
     list_len = len(arr)
     median_place = list_len // 2
     if list_len % 2 == 0:
@@ -61,18 +64,21 @@ def median (arr):
     else:
         return ValueError
 
-def stdev (arr):
+
+def stdev(arr):
     # Calculate standard deviation
     try:
         mean_out = sum(arr) / len(arr)
     except ZeroDivisionError:
         print("Unable to calculate stdev")
         sys.exit(1)
-        
-    variance = sum((x - mean_out)**2 for x in arr) / len(arr)
-    return variance**0.5
+
+    variance = sum((x - mean_out) ** 2 for x in arr) / len(arr)
+    return variance ** 0.5
+
 
 if __name__ == "__main__":
-    print(get_column("src/agrofood_data_abbreviated.csv", 0, 'United States of America', 3))
+    print(get_column("src/agrofood_data_abbreviated.csv",
+                     0, 'United States of America', 3))
 # only runs when the file is directly called
 # For testing my_utils.py independently
